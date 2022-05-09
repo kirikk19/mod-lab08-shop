40cba728987687e4ebf791db41ae60caddb7ec40
Ilya <40660169+Just-Person@users.noreply.github.com>
2022-05-09 22:02:15 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,5 +1,9 @@
 // Copyright 2021 GHA Test Team
 #include <gtest/gtest.h>
-#include "task1.h"
-#include "task2.h"
-#include "task3.h"
+#include <ctime>
+#include "market.h"
+TEST(TEST1, TrueOrFalse) {
+srand(time(nullptr));
+Supermarket* supermarket = new Supermarket(20, 40, 500, 5, 20, 1);
+EXPECT_EQ(static_cast<int> (supermarket->RelativeThroughput()), text.getprefix());
+}
