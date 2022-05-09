bd00a79089801ffd82b9b59eeacd483ead242aac
Ilya <40660169+Just-Person@users.noreply.github.com>
2022-05-09 22:18:36 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,10 +1,3 @@
 // Copyright 2021 GHA Test Team
 #include <gtest/gtest.h>
 #include <ctime>
-#include "../include/market.h"
-
-TEST(TEST1, TrueOrFalse) {
-srand(time(nullptr));
-Supermarket* supermarket = new Supermarket(20, 40, 500, 5, 20, 1);
-EXPECT_EQ(1, static_cast<int> (supermarket->RelativeThroughput()));
-}
