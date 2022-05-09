902742449ab1ec08e272fad3e3fae04d77e6d7e1
Ilya <40660169+Just-Person@users.noreply.github.com>
2022-05-09 21:52:17 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,30 @@
-﻿// SMOMarket.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-#include "market.h"
+  // Copyright 2022 UNN-IASR
 #include <ctime>
-int main()
-{
-	srand(time(nullptr));
-	Supermarket* supermarket = new Supermarket(15, 40, 10, 5);
-	supermarket->run();
-	return 0;
+#include "market.h"
+
+int main() {
+srand(time(nullptr));
+Supermarket* supermarket = new Supermarket(20, 40, 500, 5, 20, 1);
+supermarket->run();
+std::cout << "Served buyers: "
+<< supermarket->ServedBuyers() << "\n";
+std::cout << "Not served buyers: "
+<< supermarket->NotServedBuyers() << "\n";
+std::cout << "Average buyers on queue: "
+<< supermarket->AverageBuyersOnQueue() << "\n";
+std::cout << "Average time in queue: "
+<< supermarket->InQueue() << " sec\n";
+std::cout << "Average time on cash register: "
+<< supermarket->OnCashBox() << " sec\n";
+std::cout << "Time during which the cash register is idle "
+<< supermarket->TimeCashBoxStop() << " sec\n";
+std::cout << "Time during which the cash register is work "
+<< supermarket->TimeCashBoxWork() << " sec\n";
+std::cout << "Failure probability "
+<< supermarket->FailureProbability() << "\n";
+std::cout << "Relative throughput "
+<< supermarket->RelativeThroughput() << "\n";
+std::cout << "Absolute bandwidth "
+<< supermarket->AbsoluteBandwidth() << "\n";
+return 0;
 }
