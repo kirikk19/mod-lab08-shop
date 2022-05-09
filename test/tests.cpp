138cd131e48f9ec117bd73783673866b94d0b3bb
Ilya <40660169+Just-Person@users.noreply.github.com>
2022-05-09 22:27:50 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,7 +1,7 @@
 // Copyright 2021 GHA Test Team
 #include <gtest/gtest.h>
 #include <ctime>
-#include "../include/task.h"
+#include "../include/market.h"
 TEST(TEST1, ServedBuyers) {
 srand(time(nullptr));
 Supermarket* supermarket = new Supermarket(20, 40, 500, 5, 20, 1);
