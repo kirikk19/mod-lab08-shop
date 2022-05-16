6b9ccd8c5c098884b829853389c001e2bc1e3f4b
kirikk19 <100958160+kirikk19@users.noreply.github.com>
2022-05-16 23:00:42 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -4,32 +4,32 @@
 #include "../include/market.h"
 TEST(TEST1, ServedB) {
 srand(time(nullptr));
-Market* supermarket = new Supermarket(20, 40, 500, 5, 20, 1);
+Market* supermarket = new Market(20, 40, 500, 5, 20, 1);
 supermarket->run();
 ASSERT_TRUE(supermarket->ServedB() == 20);
 }
 TEST(TEST2, NServedB) {
 srand(time(nullptr));
-Market* supermarket = new Supermarket(20, 40, 500, 5, 20, 1);
+Market* supermarket = new Market(20, 40, 500, 5, 20, 1);
 supermarket->run();
 ASSERT_TRUE(supermarket->NServedB() == 0);
 }
 TEST(TEST3, InQueue) {
 srand(time(nullptr));
-Market* supermarket = new Supermarket(20, 40, 500, 5, 20, 1);
+Market* supermarket = new Market(20, 40, 500, 5, 20, 1);
 supermarket->run();
 ASSERT_TRUE(static_cast<int>(supermarket->InQueue()) == 0);
 }
 TEST(TEST4, RelThrough) {
 srand(time(nullptr));
-Market* supermarket = new Supermarket(20, 40, 500, 5, 20, 1);
+Market* supermarket = new Market(20, 40, 500, 5, 20, 1);
 supermarket->run();
 ASSERT_TRUE(static_cast<int>(supermarket->RelThrough()) == 1);
 }
 
 TEST(TEST5, AbsoluteBand) {
 srand(time(nullptr));
-Market* supermarket = new Supermarket(20, 40, 500, 5, 20, 1);
+Market* supermarket = new Market(20, 40, 500, 5, 20, 1);
 supermarket->run();
 ASSERT_TRUE(static_cast<int>(supermarket->AbsoluteBand()) == 40);
 }
